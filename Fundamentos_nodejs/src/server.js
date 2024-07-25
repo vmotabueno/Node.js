873daa56c2b4a0a93182f8c279abc027600e2450
vmotabueno <victormotabueno@gmail.com>
2024-07-25 09:39:31 -0300

    Criando um GET,POST

---
+++ Fundamentos_nodejs/src/server.js
@@ -0,0 +1,51 @@
+import http from 'http'
+
+// - http 
+// método http
+// url
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET = Buscar um recurso do back-end
+// POST = Criar um recurso no back-end 
+// PUT  = Atualizar o recursos no back-end
+// PATCH = Atualizar uma informação específica de um recurso no back-end
+// DELETE = Deletar um recurson no back-end
+
+//GET /users => Buscando usuários do back-end
+//POST /users => Criar um recurso de usuários no back-end
+
+// stateful - stateless
+
+// json - javascrip object notation
+
+// HTTP status code
+
+const users = []
+
+const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+
+    if (method === 'GET' && url === '/users') {
+        return res
+            .setHeader('content-type', 'application/json')
+            .end(JSON.stringify(users))
+        
+    } 
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        });
+
+
+       return res.writeHead(201).end()
+    } 
+
+        return res.writeHead(404).end('Not Found')
+})
+
+server.listen(3333);
