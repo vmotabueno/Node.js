775f983ad3fdda42953a99bdd831854f94fdc287
vmotabueno <victormotabueno@gmail.com>
2024-08-08 09:57:42 -0300

    Criando middleware de JSON

--- Fundamentos_nodejs/src/server.js
+++ Fundamentos_nodejs/src/server.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import { json } from './middlewares/json.js'
 
 // - http 
 // método http
@@ -26,21 +27,10 @@ const users = []
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
-    const buffers = []
-
-    for await (const chunk of req){
-        buffers.push(chunk)
-    }
-
-    try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+   await json(req , res)
 
     if (method === 'GET' && url === '/users') {
         return res
-            .setHeader('content-type', 'application/json')
             .end(JSON.stringify(users))
         
     } 
