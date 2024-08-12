5874282c46a545459eae59943a5feaa26b0e33ab
vmotabueno <victormotabueno@gmail.com>
2024-08-12 09:13:27 -0300

    Creating unique and universal ID (UUID)

--- Fundamentos_nodejs/src/server.js
+++ Fundamentos_nodejs/src/server.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
@@ -41,7 +42,7 @@ const server = http.createServer(async (req, res) => {
         const { name, email } = req.body
 
         const user = {
-            id: 1,
+            id: randomUUID(),
             name,
             email,
         }
